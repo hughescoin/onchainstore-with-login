50effe7180c76d5e30b8bca5664c0fdbfbb323b5
Patrick Hughes <patrick.hughes@coinbase.com>
2024-12-04 08:44:14 -0600

    add login and signup buttons to navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,24 +9,28 @@ import {
 import { ExternalLinkSvg } from 'src/svg/ExternalLinkSvg';
 import { MenuSvg } from 'src/svg/MenuSvg';
 import OnchainKitShopSvg from 'src/svg/OnchainKitShopSvg';
+import { useAccount } from 'wagmi';
+
 import type { NavbarLinkReact } from 'src/types';
 
+import LoginButton from '../components/LoginButton';
+import SignupButton from '../components/SignupButton';
 function NavbarLink({ link, label }: NavbarLinkReact) {
   return (
     <li
       className={cn(
         'flex cursor-pointer items-center justify-center gap-2 rounded p-1',
-        pressable.default,
+        pressable.default
       )}
     >
       <a
         href={link}
-        className="ock-text-foreground flex items-center text-xs"
-        target="_blank"
-        rel="noreferrer"
+        className='ock-text-foreground flex items-center text-xs'
+        target='_blank'
+        rel='noreferrer'
       >
         {label}
-        <span className="pl-1">
+        <span className='pl-1'>
           <ExternalLinkSvg />
         </span>
       </a>
@@ -36,30 +40,32 @@ function NavbarLink({ link, label }: NavbarLinkReact) {
 
 export default function Navbar() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const { address } = useAccount();
 
   const toggleMenu = useCallback(() => {
     setIsMenuOpen((prev) => !prev);
   }, []);
 
   return (
-    <header className="-mx-[50vw] fixed top-10 right-1/2 left-1/2 xs:h-11 w-screen border-gray-200 border-b bg-[white]">
-      <div className="mx-auto flex h-full max-w-5xl items-center px-4 py-2 lg:px-6">
-        <div className="flex w-full items-center justify-between">
-          <div className="flex items-center space-x-3">
+    <header className='-mx-[50vw] fixed top-10 right-1/2 left-1/2 xs:h-11 w-screen border-gray-200 border-b bg-[white]'>
+      <div className='flex justify-end'></div>
+      <div className='mx-auto flex h-full max-w-5xl items-center px-4 py-2 lg:px-6'>
+        <div className='flex w-full items-center justify-between'>
+          <div className='flex items-center space-x-3'>
             <OnchainKitShopSvg />
-            <span className="ock-bg-alternate ock-text-foreground rounded-sm px-2 py-0.5 font-regular text-xs">
+            <span className='ock-bg-alternate ock-text-foreground rounded-sm px-2 py-0.5 font-regular text-xs'>
               Template
             </span>
           </div>
-          <nav className="hidden md:block">
-            <ul className="flex space-x-6">
-              <NavbarLink link={TEMPLATE_LINK} label="FORK THIS TEMPLATE" />
-              <NavbarLink link={ONCHAINKIT_LINK} label="ONCHAINKIT" />
-              <NavbarLink link={TWITTER_LINK} label="X" />
-            </ul>
-          </nav>
+          <div className='flex items-center gap-3 h-8'>
+            <SignupButton className='h-8 px-4 py-1 rounded bg-slate-200 text-sm text-[#030712] hover:bg-slate-300' />
+            {!address && (
+              <LoginButton className='h-8 px-4 py-1 rounded bg-slate-200 text-sm text-[#030712] hover:bg-slate-300' />
+            )}
+          </div>
+
           <button
-            type="button"
+            type='button'
             className={cn('md:hidden', pressable.default)}
             onClick={toggleMenu}
           >
@@ -68,12 +74,12 @@ export default function Navbar() {
         </div>
       </div>
       {isMenuOpen && (
-        <div className="bg-[white] md:hidden">
-          <ul className="flex flex-col items-start space-y-2 px-4 py-2">
-            <NavbarLink link={TEMPLATE_LINK} label="FORK THIS TEMPLATE" />
-            <NavbarLink link={ONCHAINKIT_LINK} label="ONCHAINKIT" />
-            <NavbarLink link={GITHUB_LINK} label="GITHUB" />
-            <NavbarLink link={TWITTER_LINK} label="X" />
+        <div className='bg-[white] md:hidden'>
+          <ul className='flex flex-col items-start space-y-2 px-4 py-2'>
+            <NavbarLink link={TEMPLATE_LINK} label='FORK THIS TEMPLATE' />
+            <NavbarLink link={ONCHAINKIT_LINK} label='ONCHAINKIT' />
+            <NavbarLink link={GITHUB_LINK} label='GITHUB' />
+            <NavbarLink link={TWITTER_LINK} label='X' />
           </ul>
         </div>
       )}
