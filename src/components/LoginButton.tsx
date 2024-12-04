cfbacf4abfb14707285d0a0d28d85fde224a588f
Patrick Hughes <patrick.hughes@coinbase.com>
2024-12-04 08:43:40 -0600

    change styling of login and signup buttons

--- src/components/LoginButton.tsx
+++ src/components/LoginButton.tsx
@@ -1,10 +1,14 @@
 'use client';
 import WalletWrapper from './WalletWrapper';
 
-export default function LoginButton() {
+type LoginButtonProps = {
+  className?: string;
+};
+
+export default function LoginButton({ className }: LoginButtonProps) {
   return (
     <WalletWrapper
-      className='min-w-[90px]'
+      className={`${className} bg-blue-500 hover:bg-blue-600 text-white ockConnectWallet_Container min-w-[90px] shrink bg-slate-200 text-[#030712] hover:bg-slate-300'`}
       text='Log in'
       withWalletAggregator={true}
     />
--- src/components/SignupButton.tsx
+++ src/components/SignupButton.tsx
@@ -1,10 +1,14 @@
 'use client';
 import WalletWrapper from './WalletWrapper';
 
-export default function SignupButton() {
+type SignupButtonProps = {
+  className?: string;
+};
+
+export default function SignupButton({ className }: SignupButtonProps) {
   return (
     <WalletWrapper
-      className='ockConnectWallet_Container min-w-[90px] shrink bg-slate-200 text-[#030712] hover:bg-slate-300'
+      className={`${className} bg-blue-500 hover:bg-blue-600 text-white ockConnectWallet_Container min-w-[90px] shrink bg-slate-200 text-[#030712] hover:bg-slate-300'`}
       text='Sign up'
     />
   );
