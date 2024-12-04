001404eaa0df89dd1acc94874cdde098a3b080f9
Patrick Hughes <patrick.hughes@coinbase.com>
2024-12-04 08:45:01 -0600

    add z index to walletwrapper

--- src/components/WalletWrapper.tsx
+++ src/components/WalletWrapper.tsx
@@ -27,28 +27,27 @@ export default function WalletWrapper({
   text,
 }: WalletWrapperParams) {
   return (
-    <>
+    <div className='relative' style={{ isolation: 'isolate' }}>
       <Wallet>
         <ConnectWallet className={className}>
           <Avatar className='h-6 w-6' />
           <Name />
           <ConnectWalletText>{text}</ConnectWalletText>
         </ConnectWallet>
-        <WalletDropdown>
+        <WalletDropdown className='absolute top-full right-0 z-[9999] bg-white shadow-lg rounded'>
           <Identity className='px-4 pt-3 pb-2' hasCopyAddressOnClick={true}>
             <Avatar />
             <Name />
             <Address />
-            <EthBalance />
           </Identity>
-          <WalletDropdownDisconnect />
           <WalletDropdownBasename />
+          <WalletDropdownDisconnect />
           <WalletDropdownLink icon='wallet' href='https://wallet.coinbase.com'>
             Go to Wallet Dashboard
           </WalletDropdownLink>
           <WalletDropdownFundLink />
         </WalletDropdown>
       </Wallet>
-    </>
+    </div>
   );
 }
